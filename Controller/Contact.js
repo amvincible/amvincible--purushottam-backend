577b61828d1f7dab7d85ae9ce41ef9c254eaf7d9
amvincible <108141187+amvincible@users.noreply.github.com>
2022-09-19 02:20:42 +0530

    Add files via upload

--- Controller/Contact.js
+++ Controller/Contact.js
@@ -1,50 +1,16 @@
-const contact = require("../Model/Singup")
-
-
-// exports.getAdmin=async(req,res)=>{
-//     const {name,password} = req.body;
-//     const data = await admin.findOne({name,password})
-   
-//    try {
-//     if (data!=null) {
-//         res.status(200).json({
-//             success:true,
-//             data:data,
-//             message:"login data"
-//         })
-        
-//     }
-//     else{
-//         res.status(200).json({
-//             data:data,
-//             message:"failded no user found"
-//         })
-//     }
-
-//    } catch (error) {
-//             console.log(error);       
-//    }
-// }
-exports.postContact=async(req,res)=>{
-    // const {name,password} = req.body;
-    // const data = await admin.findOne({name,password})
-   
-    const data = await contact.create(req.body)
-    res.status(200).json({
-        success:true,
-        message:"form added successfully"
-    })
-
-}
-exports.getContact=async(req,res)=>{
-    // const {name,password} = req.body;
-    // const data = await admin.findOne({name,password})
-   
-    const data = await contact.find()
-    res.status(200).json({
-        success:true,
-        data:data,
-        message:"data fetched"
-    })
-
-}
+const contact = require("../Model/Singup");
+exports.postContact = async (req, res) => {
+  const data = await contact.create(req.body);
+  res.status(200).json({
+    success: true,
+    message: "form added successfully",
+  });
+};
+exports.getContact = async (req, res) => {
+  const data = await contact.find();
+  res.status(200).json({
+    success: true,
+    data: data,
+    message: "data fetched",
+  });
+};
--- Model/Singup.js
+++ Model/Singup.js
@@ -1,12 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const contact = mongoose.Schema({
-    name:{
-        type:String
-    },
-    password:{
-        type:String
-    }
-})
+  name: {
+    type: String,
+  },
+  payment: {
+    type: String,
+  },
+  mobile: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  amount: {
+    type: String,
+  },
+});
 
-module.exports = mongoose.model("contact",contact   )
+module.exports = mongoose.model("contact", contact);
